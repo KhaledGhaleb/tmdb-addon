6435850a282f69e66459e4057f9ba52bcab35ed3
Khaled Fayed <khaled.f.gh@gmail.com>
2025-08-09 07:33:23 +0300

    update package.json

--- addon/server.js
+++ addon/server.js
@@ -3,6 +3,6 @@ import { addon } from "./index.js"; // note the .js extension
 const PORT = process.env.PORT || 1337;
 
 addon.listen(PORT, function () {
-  console.log(`http:,//127.0.0.1:${PORT}/`);
+  console.log(`http://127.0.0.1:${PORT}/`);
   console.log(`Addon active on port ${PORT}.`);
 });
